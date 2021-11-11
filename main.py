786efb28184e52d2ef69662df0430f3e890c2472
Andrea Marino <am.marinoandrea@gmail.com>
2021-11-11 12:33:37 +0100

    ✨ WARC metadata parsing init

--- main.py
+++ main.py
@@ -1,9 +1,55 @@
 import logging
+import multiprocessing as mp
+import time
+from typing import Dict
 
 from src.cli import parse_cl_args
-from src.parsing import (extract_text_from_html, init_parsing,
-                         tokenize_and_tag_raw_text)
+from src.interfaces import WARCJobInformation, WARCRecordMetadata
+from src.parsing import (extract_metadata_from_warc, extract_text_from_html,
+                         init_parsing, tokenize_and_tag_raw_text)
 from src.warc import stream_records_from_warc
+
+FLUSH_THRESHOLD: int = 500
+DAEMON_SLEEP_TIME_S: float = 0.250
+
+
+def process_archive(output_dict: Dict[WARCRecordMetadata, WARCJobInformation], warc_path: str):
+    for record in stream_records_from_warc(warc_path):
+        if record is None:
+            continue
+
+        try:
+            warc_metadata = extract_metadata_from_warc(record)
+        # we are handling the case where the record is an empty string
+        except ValueError:
+            continue
+
+        output_dict[warc_metadata] = {
+            "mappings": [],
+            "is_done": False
+        }
+
+        text = extract_text_from_html(record)
+        tagged_tokens = tokenize_and_tag_raw_text(text)
+
+        # TODO: do something with our tagged tokens!
+        # ....
+        # TODO: add
+
+
+def flush_daemon(output_dict: Dict[WARCRecordMetadata, WARCJobInformation], output_path: str):
+    while True:
+        if len(output_dict) < FLUSH_THRESHOLD:
+            time.sleep(DAEMON_SLEEP_TIME_S)
+            continue
+
+        with open(output_path, 'w+') as f:
+            for warc in output_dict:
+                if not output_dict[warc]['is_done']:
+                    continue
+                for mapping in output_dict[warc]['mappings']:
+                    f.write(
+                        f'{warc.trec_id}\t{mapping.named_entity}\t{mapping.entity_url}\n')
 
 
 def init():
@@ -12,27 +58,34 @@ def init():
     """
     logging.basicConfig(
         format='%(asctime)s [%(levelname)s]: %(message)s',
-        level=logging.DEBUG)
+        level=logging.INFO)
     logging.info('initializing NLTK dependencies')
     init_parsing()
 
 
 def main():
+    archives, output_path = parse_cl_args()
+    logging.info('parsed list of archives')
+
     init()
     logging.info('initialization completed')
 
-    archives = parse_cl_args()
-    logging.info('parsed list of archives')
+    # multiprocessing setup
+    manager = mp.Manager()
+    shared_dict = manager.dict()
+    processes: list[mp.Process] = []
+
+    # start daemon which flushes linked entities to file periodically
+    mp.Process(target=flush_daemon, args=(shared_dict, output_path)).start()
 
     for warc in archives:
-        # TODO: this for-loop will change whenever we introduce multiprocessing
-        for record in stream_records_from_warc(warc):
-            text = extract_text_from_html(record)
-            tagged_tokens = tokenize_and_tag_raw_text(text)
+        logging.info(f'starting job for archive \'{warc}\'')
+        process = mp.Process(target=process_archive, args=(shared_dict, warc))
+        process.start()
+        processes.append(process)
 
-            # TODO: do something with our tagged tokens!
-            # ....
-        logging.info(f'extracted all records from archive \'{warc}\'')
+    for process in processes:
+        process.join()
 
 
 if __name__ == '__main__':
---
+++ src/interfaces.py
@@ -0,0 +1,46 @@
+import datetime
+from dataclasses import dataclass
+from typing import Optional
+
+from typing_extensions import TypedDict
+
+
+@dataclass
+class EntityMapping:
+    named_entity: str
+    entity_url: str
+
+
+@dataclass(eq=True, frozen=True, unsafe_hash=False)
+class WARCRecordMetadata:
+    """
+    WARC record specific information is stored in this struct.
+    Since we use the Trec ID as primary key for most operations
+    this is the only required field.
+    Example of warc record metadata: 
+
+    ```
+    WARC-Type: response, 
+    WARC-Date: 2012-02-10T21:49:38Z, 
+    WARC-TREC-ID: clueweb12-0000tw-00-00010, 
+    WARC-IP-Address: 100.42.59.11
+    WARC-Payload-Digest: sha1:3I42H3S6NNFQ2MSVX7XZKYAYSCX5QBYJ, 
+    WARC-Target-URI: http://claywginn.com/wp-content/plugins/wordpress-file-monitor/wordpress-file-monitor.php, 
+    WARC-Record-ID: <urn:uuid:f638c914-658d-418a-93f8-6e89a4858359>
+    ```
+    """
+    trec_id: str
+    w_type: Optional[str] = None
+    date: Optional[datetime.datetime] = None
+    ip_addr: Optional[str] = None
+    digest: Optional[str] = None
+    uri: Optional[str] = None
+    record_id: Optional[str] = None
+
+
+class WARCJobInformation(TypedDict):
+    """
+    Utility class used by the multiprocessing Manager dict.
+    """
+    mappings: list[EntityMapping]
+    is_done: bool
